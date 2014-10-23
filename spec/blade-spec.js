d10637132ce73b503314d6b9eee403d9e4469fc4
Brian Di Palma <offler@gmail.com>
2014-10-23 14:42:40 +0100

    chore(tests): use Jasmine and ES6 for tests.

---
+++ spec/blade-spec.js
@@ -0,0 +1,10 @@
+describe('Blade analyzer', () => {
+	it('discovers all source files in a blade.', () => {
+		//Given.
+		expect(true).toBe(true);
+		//When.
+		//Then.
+	});
+});
+
+export var t = 'WAT';
--- test/branalyzer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var assert = require('assert');
-
-describe('A test that passes', function() {
-	it('should pass.', function() {
-		assert.equal(1, 1);
-	});
-});
