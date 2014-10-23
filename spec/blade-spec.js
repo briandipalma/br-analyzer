667becdfada4c38e3056e510c2e2a825e2d32469
Brian Di Palma <offler@gmail.com>
2014-10-23 16:44:08 +0100

    test(blade): let's make async code appear sync.

--- spec/blade-spec.js
+++ spec/blade-spec.js
@@ -1,24 +1,26 @@
 const assert = require('assert');
 
+const bluebird = require('bluebird');
 import {getBladeSourceFileNames} from '../src/blade';
 
 describe('Blade analyzer', () => {
 	it('discovers all source files in a blade.', (done) => {
-		//Given.
-		const testBladeDirectoryName = 'spec/resources/test-bladeset/blades/test-blade/';
-		const expectedBladeFileNames = [
-			'spec/resources/test-bladeset/blades/test-blade/src/my/name/space/MyClass.js',
-			'spec/resources/test-bladeset/blades/test-blade/src/my/name/space/sub/MySubClass.js'
-		];
-
-		//When.
-		const bladeFileNamesPromise = getBladeSourceFileNames(testBladeDirectoryName);
-
-		//Then.
-		bladeFileNamesPromise
-			.then((bladeFileNames) => {
-				assert.deepEqual(bladeFileNames, expectedBladeFileNames);
-			})
-			.then(() => done());
+		bluebird.coroutine(discoverAllSourceFilesInABlade)(done);
 	});
 });
+
+function* discoverAllSourceFilesInABlade(done) {
+	//Given.
+	const testBladeDirectoryName = 'spec/resources/test-bladeset/blades/test-blade/';
+	const expectedBladeFileNames = [
+		'spec/resources/test-bladeset/blades/test-blade/src/my/name/space/MyClass.js',
+		'spec/resources/test-bladeset/blades/test-blade/src/my/name/space/sub/MySubClass.js'
+	];
+
+	//When.
+	const bladeFileNames = yield getBladeSourceFileNames(testBladeDirectoryName);
+
+	//Then.
+	assert.deepEqual(bladeFileNames, expectedBladeFileNames);
+	done();
+}
