d3cebce4099d41aed1667aee22d32e4775424807
Brian Di Palma <offler@gmail.com>
2014-10-27 18:05:00 +0000

    fix(blade tests): Jasmine now supports Promises, stop using the `done` callback

--- spec/blade-spec.js
+++ spec/blade-spec.js
@@ -8,12 +8,11 @@ import {
 } from './test-constants';
 
 describe('Blade analyzer', () => {
-	it('discovers all source files in a blade.', wrap(function* (done) {
+	it('discovers all source files in a blade.', wrap(function* () {
 		//When.
 		const bladeFileNames = yield getBladeSourceFileNames(testBladeDirectoryName);
 
 		//Then.
 		assert.deepEqual(bladeFileNames, expectedBladeFileNames);
-		done();
 	}));
 });
